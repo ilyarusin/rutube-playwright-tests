b14458561dfd40bb1d5b80d83d84454a3b856b81
Ilya Piminov <ilyapiminov@gmail.com>
2026-04-22 16:03:50 +0500

    добавлен файл fixtures

--- tests/specs/unauthorized/mainPage.spec.ts
+++ tests/specs/unauthorized/mainPage.spec.ts
@@ -1,28 +1,13 @@
-import test from "@playwright/test";
-import { MainPage } from "../../pages/MainPage";
+import {test, expect} from "../../fixtures/fixtures";
 
-test("Открытие главной страницы", async ({ page }) => {
-  const mainPage = new MainPage(page);
-  await mainPage.open();
-});
-
-test("Проверка доступности элементов хедера", async ({ page }) => {
-  const mainPage = new MainPage(page);
-  await mainPage.open();
-  await mainPage.closeCookiesAlert();
+test("Проверка доступности элементов хедера", async ({ mainPage }) => {
   await mainPage.headerHasCorrectAriaSnapshot();
 });
 
-test("Проверка доступности табов категорий", async ({ page }) => {
-  const mainPage = new MainPage(page);
-  await mainPage.open();
-  await mainPage.closeCookiesAlert();
+test("Проверка доступности табов категорий", async ({ mainPage }) => {
   await mainPage.categoriesTabsHasCorrectAriaSnapshot();
 });
 
-test("Проверка доступности элементов бокового меню", async ({ page }) => {
-  const mainPage = new MainPage(page);
-  await mainPage.open();
-  await mainPage.closeCookiesAlert();
+test("Проверка доступности элементов бокового меню", async ({ mainPage }) => {
   await mainPage.menuHasCorrectAriaSnapshot();
 });
