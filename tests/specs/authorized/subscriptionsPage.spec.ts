5872ac9bddafa09af5a4e66f90e9f6e59e48cafb
Ilya Piminov <ilyapiminov@gmail.com>
2026-04-27 18:26:10 +0500

    subscriptions page tests

---
+++ tests/specs/authorized/subscriptionsPage.spec.ts
@@ -0,0 +1,10 @@
+import test from "@playwright/test";
+import { SubscriptionsPage } from "../../pages/SubscriptionsPage";
+
+test('Проверка доступности контента для авторизованного пользователя', async ({page}) => {
+    const subscriptionsPage = new SubscriptionsPage(page);
+    await subscriptionsPage.open();
+    await subscriptionsPage.closeCookiesAlert();
+    await subscriptionsPage.closePopUP();
+    await subscriptionsPage.contentHasCorrectAriaSnapshot();
+})
